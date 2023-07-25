44e398438f0b4d15faf88781a79120a66b1cf6e2
vasil14 <vasil_v@hotmail.com>
2023-07-25 13:25:08 +0200

    WIP-Addign routes and navbar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/login/Login';
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/login/Login";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <>
+      {location.pathname.match("/login") ||
+      location.pathname.match("/register") ? (
+        ""
+      ) : (
+        <Navbar />
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register'/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" />
       </Routes>
     </>
   );
