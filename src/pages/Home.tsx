bd9c069d0831859f9989f9a764c793383c682d2b
vasil14 <vasil_v@hotmail.com>
2023-07-27 20:16:27 +0200

    WIP-testing routes from be

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,46 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
+interface AuthorAttributes {
+  name: string
+  bio: string
+  birthday: string
+}
 interface BookAttributes {
-  id: number;
-  title: string;
-  descripton: string;
-  slug: string;
+  id: number
+  title: string
+  descripton: string
+  slug: string
+  Authors: AuthorAttributes[]
 }
 const Home = () => {
-  const [books, setBooks] = useState<BookAttributes[]>([]);
-  console.log(books);
+  const [books, setBooks] = useState<BookAttributes[]>([])
+  console.log(books)
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/books")
+      .get('http://localhost:3000/books')
       .then((response) => setBooks(response.data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+      .catch((error) => console.error('Error fetching data:', error))
+  }, [])
 
   return (
-    <div>
-      {books?.map((book) => {
-        return <div key={book.slug}>{book.title}</div>;
-      })}
+    <div className="max-w-screen-lg mx-auto mt-10">
+      <div className="grid grid-cols-5 w-full mt- border">
+        {books?.map((book) => {
+          return (
+            <div
+              key={book.slug}
+              className={`border-r border-b mx-auto px-5 h-52 w-full`}
+            >
+              <div>Title: {book.title}</div>
+              <div>Author: {book.Authors[0]?.name}</div>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
